c99978ca9cce439f93633e623bea683b7ce6af7d
Unknown <pe0877888@otc.edu>
2017-10-02 21:18:43 -0500

    buttons work

--- likes.js
+++ likes.js
@@ -1,12 +1,21 @@
- function onClick(){
-     let clicks = document.getElementById('likes').nextSibling
-    let likes = parseInt(clicks.textContent) + 1
-    clicks.textContent = likes
+// function onClick(){
+//      let clicks = document.getElementById('tweets').nextSibling   
+//      let tweets = parseInt(clicks.textContent) + 1
+//     clicks.textContent = tweets
 
- };
+//  };
 
- let buttons = document.getElementById('likebutton')
- buttons.addEventListener('click', onClick)
+//  let buttons = document.getElementById('retweet')
+
+//  buttons.addEventListener('click', onClick)
 
 
-              
+let buttons = Array.from(document.getElementsByClassName("glyphicon"));
+buttons.forEach(button => {
+    button.addEventListener("click", e => {
+        let currentNum = button.nextSibling.textContent.trim()
+        let likes = parseInt(currentNum) + 1;
+        button.nextSibling.textContent = likes;
+})
+
+})
