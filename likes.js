f227fd641476e3482377a50e1f172bf7e31681e1
Cierra Strickland <cs0856663@otc.edu>
2017-10-04 13:21:26 -0500

    jquery UGHH

--- likes.js
+++ likes.js
@@ -10,12 +10,24 @@
 //  buttons.addEventListener('click', onClick)
 
 
-let buttons = Array.from(document.getElementsByClassName("glyphicon"));
-buttons.forEach(button => {
-    button.addEventListener("click", e => {
-        let currentNum = button.nextSibling.textContent.trim()
+// let buttons = Array.from(document.getElementsByClassName("glyphicon"));
+// buttons.forEach(button => {
+//     button.addEventListener("click", e => {
+//         let currentNum = button.nextSibling.textContent.trim()
+//         let likes = parseInt(currentNum) + 1;
+//         button.nextSibling.textContent = likes;
+// })
+
+// })
+
+$(function() {
+
+    $('.likes').click(event =>{
+        let currentNum = $(event.target).text()
         let likes = parseInt(currentNum) + 1;
-        button.nextSibling.textContent = likes;
-})
+
+        
+
+    })
 
 })
