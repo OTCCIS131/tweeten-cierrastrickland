29dd4bb5c0dcd4d14a79350808e2471cd83f8219
Cierra Strickland <cs0856663@otc.edu>
2017-10-09 12:56:13 -0500

    switched to javaScript pt.2

--- likes.js
+++ likes.js
@@ -1,15 +1,3 @@
-function onClick(){
-     let clicks = document.getElementById('tweets').nextSibling   
-     let tweets = parseInt(clicks.textContent) + 1
-    clicks.textContent = tweets
-
- };
-
- let buttons = document.getElementById('retweet')
-
- buttons.addEventListener('click', onClick)
-
-
 let buttons = Array.from(document.getElementsByClassName("glyphicon"));
 buttons.forEach(button => {
     button.addEventListener("click", e => {
